201856d2a2143474a8d84bd341743549254ee5dc
Cristian Villafane <cristianv129@hotmail.com>
2020-12-08 07:28:23 -0300

    algo

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -4,6 +4,8 @@ export default axios.create({
     baseURL: process.env.REACT_APP_SERVER || "http://localhost:8080/api",
     headers: {
         "Content-type": "application/json",
-        'Access-Control-Allow-Origin' : '*'
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':  "POST, GET, OPTIONS, DELETE, PUT",
+        'Access-Control-Allow-Headers': "append,delete,entries,foreach,get,has,keys,set,values,Authorization"
     }
 });
